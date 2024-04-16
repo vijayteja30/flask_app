522cd456972aaee7c3b779e4573cfb61a206f864
vijayteja30 <vijayteja30@gmail.com>
2024-04-16 22:35:30 +0530

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -32,6 +32,9 @@ def about():
 def hello_world(value):
     return f'Hello World Value {value}'
 
+@app.route('/live',methods = ['GET'])
+def root_page():
+    return "Api Trigger Success"
 
 if __name__ == "__main__":
     app.run(debug=True)
